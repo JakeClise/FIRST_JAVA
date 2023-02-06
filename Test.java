3732bd02e641c7e5812bb6df8a33569c7f8d1108
JakeClise <jhclise@gmail.com>
2023-02-05 23:54:39 -0500

    first commit

---
+++ Test.java
@@ -0,0 +1,14 @@
+public class Test { 
+    public static void main(String[] args) {
+        System.out.println("HelloWorld!");
+    }
+}
+
+//every file is a class
+//class name must be the same as the file name
+//file & class name should be in PascalCase -- ex: BankAccount.java
+//think of main as the launchpad for all applications
+//in java, semicolons are required syntax for ending any statement
+//TO COMPILE: javac NameOfFile.java
+//TO RUN PROGRAM: java NameOfFile
+//"WHENEVER YOU RUN A FILE, YOU GOTTA COMPILE"
